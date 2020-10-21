a8d3c8d064824376d22a4ac7b5eff7f44bda53b5
Administrator <835683840@qq.com>
2020-10-21 11:11:42 +0800

    添加switchbutton

---
+++ app/src/main/java/com/example/ui/SwitchButtonActivity.java
@@ -0,0 +1,30 @@
+package com.example.ui;
+
+
+import android.os.Bundle;
+
+import com.gc.common.ui.BaseActivity;
+
+public class SwitchButtonActivity extends BaseActivity {
+
+
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_switch_button;
+    }
+
+    @Override
+    public void initView(Bundle savedInstanceState) {
+        setBackTitle("switch_button");
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+
+
+
+}
